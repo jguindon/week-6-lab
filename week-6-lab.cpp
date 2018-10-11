3bace6aee11a255e47ac20746fb4bb99bd7d5212
jguindon <43012863+jguindon@users.noreply.github.com>
2018-10-11 14:33:27 -0400

    Week 6 C++ lab

---
+++ week-6-lab.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int score;    
+    cout <<"Please enter your score:" << endl;
+    cin >> score;
+
+if      (score >= 86)
+    cout << "Grade is A";
+else if (score >=72)
+    cout << "Grade is B";
+else if (score >=60)
+    cout << "Grade is C";
+else if (score >=50)
+    cout << "Grade is D";
+else 
+    cout <<"Grade is F";
+    
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
